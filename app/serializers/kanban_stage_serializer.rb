fa617db7c7380ed3e57859c57a9bc37b006cdddb
doni010520 <donikasumii@gmail.com>
2026-02-14 12:36:24 -0300

    Delete app/serializers/kanban_stage_serializer.rb

--- app/serializers/kanban_stage_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class KanbanStageSerializer < ActiveModel::Serializer
-  attributes :id,
-             :name,
-             :position,
-             :color,
-             :kanban_pipeline_id
-end
