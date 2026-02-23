540151f062a3b7f1922b5bad7001539d60f8bd4f
doni010520 <donikasumii@gmail.com>
2026-02-23 13:21:11 -0300

    Update editorHelper.js

--- app/javascript/dashboard/helper/editorHelper.js
+++ app/javascript/dashboard/helper/editorHelper.js
@@ -110,7 +110,7 @@ export function cleanSignature(signature) {
  * @returns {string} - The signature with the delimiter added.
  */
 function appendDelimiter(signature) {
-  return `${SIGNATURE_DELIMITER}\n\n${cleanSignature(signature)}`;
+  return cleanSignature(signature);
 }
 
 /**
