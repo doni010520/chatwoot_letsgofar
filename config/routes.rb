a47d9261f898ffd1767aed559fec157697e5ca4a
doni010520 <donikasumii@gmail.com>
2026-03-04 15:05:20 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -399,8 +399,7 @@ Rails.application.routes.draw do
 
           # Kanban
           namespace :kanban do
-          # Kanban
-          namespace :kanban do
+       
             # Tarefas do usuário (global)
             resource :user_tasks, only: [], controller: 'user_tasks' do
               collection do
