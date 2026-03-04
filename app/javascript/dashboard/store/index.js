980c8f207a7c56e63aff1259a149a979b94e5d12
doni010520 <donikasumii@gmail.com>
2026-03-04 14:49:37 -0300

    Update index.js

--- app/javascript/dashboard/store/index.js
+++ app/javascript/dashboard/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from 'vuex';
 import accounts from './modules/accounts';
 import agentBots from './modules/agentBots';
 import agentCapacityPolicies from './modules/agentCapacityPolicies';
+import agentTasks from './modules/agentTasks';
 import agents from './modules/agents';
 import assignmentPolicies from './modules/assignmentPolicies';
 import articles from './modules/helpCenterArticles';
@@ -69,6 +70,7 @@ export default createStore({
     accounts,
     agentBots,
     agentCapacityPolicies,
+    agentTasks,
     agents,
     assignmentPolicies,
     articles,
