016200324700bbb7cb048c95bef42d5e32ceb7fa
doni010520 <donikasumii@gmail.com>
2026-03-05 00:05:24 -0300

    Update scheduledMessages.js

--- app/javascript/dashboard/store/modules/scheduledMessages.js
+++ app/javascript/dashboard/store/modules/scheduledMessages.js
@@ -24,6 +24,28 @@ const actions = {
     }
   },
 
+  async create({ commit }, data) {
+    commit('setUIFlag', { isCreating: true });
+    try {
+      const response = await ScheduledMessagesAPI.create(data);
+      commit('addMessage', response.data);
+      return response;
+    } finally {
+      commit('setUIFlag', { isCreating: false });
+    }
+  },
+
+  async update({ commit }, { id, data }) {
+    commit('setUIFlag', { isUpdating: true });
+    try {
+      const response = await ScheduledMessagesAPI.update(id, data);
+      commit('updateMessage', response.data);
+      return response;
+    } finally {
+      commit('setUIFlag', { isUpdating: false });
+    }
+  },
+
   async delete({ commit }, id) {
     await ScheduledMessagesAPI.delete(id);
     commit('deleteMessage', id);
@@ -33,6 +55,17 @@ const actions = {
 const mutations = {
   setScheduledMessages(state, messages) {
     state.records = messages;
+  },
+
+  addMessage(state, message) {
+    state.records.push(message);
+  },
+
+  updateMessage(state, updatedMessage) {
+    const index = state.records.findIndex(msg => msg.id === updatedMessage.id);
+    if (index !== -1) {
+      state.records[index] = updatedMessage;
+    }
   },
 
   deleteMessage(state, id) {
