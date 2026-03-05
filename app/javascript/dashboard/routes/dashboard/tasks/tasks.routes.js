1d73d473e6c46885ea8794ae5411bf6aee3fed4c
doni010520 <donikasumii@gmail.com>
2026-03-05 07:43:48 -0300

    Update tasks.routes.js

--- app/javascript/dashboard/routes/dashboard/tasks/tasks.routes.js
+++ app/javascript/dashboard/routes/dashboard/tasks/tasks.routes.js
@@ -1,3 +1,5 @@
+import { frontendURL } from '../../../helper/URLHelper';
+
 const TasksView = () => import('./TasksView.vue');
 const TaskList = () => import('./components/TaskList.vue');
 const TaskCalendar = () => import('./components/TaskCalendar.vue');
@@ -5,40 +7,40 @@ const TaskKanban = () => import('./components/TaskKanban.vue');
 
 const TASKS_PERMISSIONS = ['administrator', 'agent', 'custom_role'];
 
-export const routes = [
-  {
-    path: 'tasks',
-    component: TasksView,
-    meta: {
-      permissions: TASKS_PERMISSIONS,
+export default {
+  routes: [
+    {
+      path: frontendURL('accounts/:accountId/tasks'),
+      component: TasksView,
+      meta: {
+        permissions: TASKS_PERMISSIONS,
+      },
+      children: [
+        {
+          path: '',
+          name: 'tasks_list',
+          component: TaskList,
+          meta: {
+            permissions: TASKS_PERMISSIONS,
+          },
+        },
+        {
+          path: 'calendar',
+          name: 'tasks_calendar',
+          component: TaskCalendar,
+          meta: {
+            permissions: TASKS_PERMISSIONS,
+          },
+        },
+        {
+          path: 'kanban',
+          name: 'tasks_kanban',
+          component: TaskKanban,
+          meta: {
+            permissions: TASKS_PERMISSIONS,
+          },
+        },
+      ],
     },
-    children: [
-      {
-        path: '',
-        name: 'tasks_list',
-        component: TaskList,
-        meta: {
-          permissions: TASKS_PERMISSIONS,
-        },
-      },
-      {
-        path: 'calendar',
-        name: 'tasks_calendar',
-        component: TaskCalendar,
-        meta: {
-          permissions: TASKS_PERMISSIONS,
-        },
-      },
-      {
-        path: 'kanban',
-        name: 'tasks_kanban',
-        component: TaskKanban,
-        meta: {
-          permissions: TASKS_PERMISSIONS,
-        },
-      },
-    ],
-  },
-];
-
-export default routes;
+  ],
+};
