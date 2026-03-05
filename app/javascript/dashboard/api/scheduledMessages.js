f7ae8aa060d86e87e544c33f2733bbe1125e43b4
doni010520 <donikasumii@gmail.com>
2026-03-05 00:05:55 -0300

    Update scheduledMessages.js

--- app/javascript/dashboard/api/scheduledMessages.js
+++ app/javascript/dashboard/api/scheduledMessages.js
@@ -9,6 +9,14 @@ class ScheduledMessagesAPI extends ApiClient {
     return window.axios.get(this.url, { params: filters });
   }
 
+  create(data) {
+    return window.axios.post(this.url, data);
+  }
+
+  update(id, data) {
+    return window.axios.put(`${this.url}/${id}`, data);
+  }
+
   delete(id) {
     return window.axios.delete(`${this.url}/${id}`);
   }
