9b9afb8bcdaa4233f09a2e1f19faf458596e270a
doni010520 <donikasumii@gmail.com>
2026-03-10 07:10:28 -0300

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -103,6 +103,8 @@ class Account < ApplicationRecord
   has_many :canned_responses, dependent: :destroy_async
   has_many :categories, dependent: :destroy_async, class_name: '::Category'
   has_many :contacts, dependent: :destroy_async
+  has_many :contracts, dependent: :destroy_async
+  has_many :contract_templates, dependent: :destroy_async
   has_many :conversations, dependent: :destroy_async
   has_many :csat_survey_responses, dependent: :destroy_async
   has_many :custom_attribute_definitions, dependent: :destroy_async
