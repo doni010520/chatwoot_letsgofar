c1e5830fe8ad5e2dae09318093751f3d15f585d9
doni010520 <donikasumii@gmail.com>
2026-03-12 10:58:38 -0300

    Update Logo.vue

--- app/javascript/dashboard/components-next/icon/Logo.vue
+++ app/javascript/dashboard/components-next/icon/Logo.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { useAttrs } from 'vue';
 import { useMapGetter } from 'dashboard/composables/store';
-import logoImage from 'dashboard/assets/images/letsgofar-logo.png';
 
 const attrs = useAttrs();
 const globalConfig = useMapGetter('globalConfig/get');
@@ -9,14 +8,8 @@ const globalConfig = useMapGetter('globalConfig/get');
 
 <template>
   <img
-    v-if="globalConfig.logoThumbnail"
     v-bind="attrs"
-    :src="globalConfig.logoThumbnail"
-  />
-  <img
-    v-else
-    v-bind="attrs"
-    :src="logoImage"
+    :src="globalConfig.logo"
     alt="Let's Go Far"
   />
 </template>
