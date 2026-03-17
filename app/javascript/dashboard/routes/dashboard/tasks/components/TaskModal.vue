3fc2f4adb958ec3e635d5abc40e2cd5a94f811a1
doni010520 <donikasumii@gmail.com>
2026-03-17 19:22:35 -0300

    Update TaskModal.vue

--- app/javascript/dashboard/routes/dashboard/tasks/components/TaskModal.vue
+++ app/javascript/dashboard/routes/dashboard/tasks/components/TaskModal.vue
@@ -5,14 +5,6 @@ import { useI18n } from 'vue-i18n';
 
 import Button from 'dashboard/components-next/button/Button.vue';
 import Modal from 'dashboard/components/Modal.vue';
-
-const formatFileSize = (bytes) => {
-  if (bytes === 0) return '0 Bytes';
-  const k = 1024;
-  const sizes = ['Bytes', 'KB', 'MB', 'GB'];
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
-};
   
 const props = defineProps({
   task: {
