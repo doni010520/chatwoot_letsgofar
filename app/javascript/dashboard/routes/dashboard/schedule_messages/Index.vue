5172ccb7bff57ec4cf9f8ca5acb29cc16293a8d6
doni010520 <donikasumii@gmail.com>
2026-03-19 09:21:54 -0300

    Update Index.vue

--- app/javascript/dashboard/routes/dashboard/schedule_messages/Index.vue
+++ app/javascript/dashboard/routes/dashboard/schedule_messages/Index.vue
@@ -1455,8 +1455,8 @@ export default {
       color: var(--s-200);
 
       option {
-        background: var(--s-900);
-        color: var(--s-200);
+        background: var(--s-900) !important;
+        color: var(--s-200) !important;
       }
     }
 
