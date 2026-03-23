dc0e177fea6927461752820f0991c64f27942bf6
doni010520 <donikasumii@gmail.com>
2026-03-23 18:01:29 -0300

    Update LabelItem.vue

--- app/javascript/dashboard/components-next/Label/LabelItem.vue
+++ app/javascript/dashboard/components-next/Label/LabelItem.vue
@@ -12,16 +12,17 @@ const props = defineProps({
   },
 });
 
-const emit = defineEmits(['remove', 'hover']);
+const emit = defineEmits(['remove', 'hover', 'edit']);
 
 const handleRemoveLabel = () => {
   emit('remove', props.label);
 };
 
+const handleEditLabel = () => {
+  emit('edit', props.label);
+};
+
 const handleMouseEnter = () => {
-  // Notify parent component when this label is hovered
-  // Added this to show the remove button with transition when hovering over the label
-  // This will solve the flickering issue when hovering over the last label item
   emit('hover', props.label?.id);
 };
 </script>
@@ -40,8 +41,20 @@ const handleMouseEnter = () => {
     </span>
     <div
       class="w-0 flex relative ltr:left-1 rtl:right-1 flex-shrink-0 overflow-hidden transition-[width] duration-300 ease-out"
-      :class="{ 'w-6': isHovered }"
+      :class="{ 'w-12': isHovered }"
     >
+      <!-- Botão Editar -->
+      <Button
+        class="transition-opacity duration-200 !h-7 w-6 bg-transparent"
+        :class="{ 'opacity-0': !isHovered, 'opacity-100': isHovered }"
+        type="button"
+        slate
+        xs
+        faded
+        icon="i-lucide-pencil"
+        @click="handleEditLabel"
+      />
+      <!-- Botão Remover -->
       <Button
         class="transition-opacity duration-200 !h-7 ltr:rounded-r-md rtl:rounded-l-md ltr:rounded-l-none rtl:rounded-r-none w-6 bg-transparent"
         :class="{ 'opacity-0': !isHovered, 'opacity-100': isHovered }"
