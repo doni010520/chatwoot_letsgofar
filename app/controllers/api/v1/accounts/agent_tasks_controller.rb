0762aa9a78ce7587a44057f4c7e9610fa1cba3de
doni010520 <donikasumii@gmail.com>
2026-04-09 15:18:29 -0300

    Remove redundant current_user assignments
    Removed unnecessary assignment of current_user in multiple methods.

--- app/controllers/api/v1/accounts/agent_tasks_controller.rb
+++ app/controllers/api/v1/accounts/agent_tasks_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::Accounts::AgentTasksController < Api::V1::Accounts::BaseControlle
   def create
     @agent_task = Current.account.agent_tasks.new(agent_task_params)
     @agent_task.created_by = Current.user
-    @agent_task.current_user = Current.user
 
     if @agent_task.save
       Rails.configuration.dispatcher.dispatch(Events::Types::AGENT_TASK_CREATED, Time.zone.now, agent_task: @agent_task)
@@ -37,7 +36,6 @@ class Api::V1::Accounts::AgentTasksController < Api::V1::Accounts::BaseControlle
     # Separar files dos outros parâmetros
     params_hash = agent_task_params.to_h
     files_to_attach = params_hash.delete('files')
-    @agent_task.current_user = Current.user
     
     # Atualizar tarefa SEM files
     if @agent_task.update(params_hash)
@@ -59,35 +57,30 @@ class Api::V1::Accounts::AgentTasksController < Api::V1::Accounts::BaseControlle
   # Ações customizadas
   def complete
     authorize @agent_task
-    @agent_task.current_user = Current.user
     @agent_task.complete!
     render :show
   end
 
   def start
     authorize @agent_task
-    @agent_task.current_user = Current.user
     @agent_task.start!
     render :show
   end
 
   def cancel
     authorize @agent_task
-    @agent_task.current_user = Current.user
     @agent_task.cancel!
     render :show
   end
 
   def reopen
     authorize @agent_task
-    @agent_task.current_user = Current.user
     @agent_task.reopen!
     render :show
   end
 
   def assign
     authorize @agent_task
-    @agent_task.current_user = Current.user
     
     user_id = params[:user_id]
     if user_id.present?
@@ -103,7 +96,6 @@ class Api::V1::Accounts::AgentTasksController < Api::V1::Accounts::BaseControlle
   
   def remove_file
     authorize @agent_task
-    @agent_task.current_user = Current.user
     
     begin
       # Buscar attachment diretamente pelo ID
